f9afcef0ec9162381772fd1f032d07c9e55c30cb
mandykr <hongseokii92@gmail.com>
2022-04-18 15:01:12 +0900

    Initial commit

---
+++ src/main/java/com/example/nutrient/NutrientAtddApplication.java
@@ -0,0 +1,13 @@
+package com.example.nutrient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrientAtddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutrientAtddApplication.class, args);
+    }
+
+}
