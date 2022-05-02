148d7c125e6e0a8477d6554dcad94f47f059f17a
mandykr <hongseokii92@gmail.com>
2022-05-02 11:37:26 +0900

    feat(combination): HealthCondition 추가

---
+++ src/main/java/com/example/nutrient/domain/HealthConditionName.java
@@ -0,0 +1,40 @@
+package com.example.nutrient.domain;
+
+import org.apache.logging.log4j.util.Strings;
+
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+public class HealthConditionName {
+    private static final String HEALTH_CONDITION_NAME_MUST_NOT_BE_NULL = "건강 상태 이름은 비어있지 않아야 합니다.";
+    private final String name;
+
+    public HealthConditionName() {
+        this.name = null;
+    }
+
+    public HealthConditionName(String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    private void validate(String name) {
+        if (Strings.isEmpty(name)) {
+            throw new IllegalArgumentException(HEALTH_CONDITION_NAME_MUST_NOT_BE_NULL);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HealthConditionName that = (HealthConditionName) o;
+        return name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
