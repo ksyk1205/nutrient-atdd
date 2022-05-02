b82b46df44d8e960c11de9acb24310e7e3543f37
mandykr <hongseokii92@gmail.com>
2022-05-02 11:56:33 +0900

    feat(combination): 오류 메세지 변경

--- src/main/java/com/example/nutrient/domain/CombinationTitle.java
+++ src/main/java/com/example/nutrient/domain/CombinationTitle.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 
 @Embeddable
 public class CombinationTitle {
-    private static final String COMBINATION_TITLE_MUST_NOT_BE_NULL = "조합 제목은 비어있지 않아야 합니다.";
+    private static final String COMBINATION_TITLE_MUST_NOT_BE_EMPTY = "조합 제목은 비어있지 않아야 합니다.";
     private final String title;
 
     protected CombinationTitle() {
@@ -21,7 +21,7 @@ public class CombinationTitle {
 
     private void validate(String title) {
         if (Strings.isEmpty(title)) {
-            throw new IllegalArgumentException(COMBINATION_TITLE_MUST_NOT_BE_NULL);
+            throw new IllegalArgumentException(COMBINATION_TITLE_MUST_NOT_BE_EMPTY);
         }
     }
 
--- src/main/java/com/example/nutrient/domain/HealthConditionName.java
+++ src/main/java/com/example/nutrient/domain/HealthConditionName.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 
 @Embeddable
 public class HealthConditionName {
-    private static final String HEALTH_CONDITION_NAME_MUST_NOT_BE_NULL = "건강 상태 이름은 비어있지 않아야 합니다.";
+    private static final String HEALTH_CONDITION_NAME_MUST_NOT_BE_EMPTY = "건강 상태 이름은 비어있지 않아야 합니다.";
     private final String name;
 
     protected HealthConditionName() {
@@ -21,7 +21,7 @@ public class HealthConditionName {
 
     private void validate(String name) {
         if (Strings.isEmpty(name)) {
-            throw new IllegalArgumentException(HEALTH_CONDITION_NAME_MUST_NOT_BE_NULL);
+            throw new IllegalArgumentException(HEALTH_CONDITION_NAME_MUST_NOT_BE_EMPTY);
         }
     }
 
