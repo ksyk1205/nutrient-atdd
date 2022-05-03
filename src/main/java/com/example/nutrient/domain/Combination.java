b1725b9faa154eeec4d60785b86aa97835985b16
mandykr <hongseokii92@gmail.com>
2022-05-03 10:30:30 +0900

    feat(combination): Combination 추가

--- src/main/java/com/example/nutrient/domain/Combination.java
+++ src/main/java/com/example/nutrient/domain/Combination.java
@@ -1,12 +1,10 @@
 package com.example.nutrient.domain;
 
-import lombok.Getter;
-
 import javax.persistence.*;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
-@Getter
 public class Combination {
     @Column(name = "id", columnDefinition = "varbinary(16)")
     @Id
@@ -15,10 +13,44 @@ public class Combination {
     @Embedded
     private CombinationTitle title;
 
-    @Lob
-    private String content;
+    @Embedded
+    private CombinationContent content;
 
+    @Enumerated(EnumType.STRING)
     private Gender recommendedGender;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "health_condition_id",
+            columnDefinition = "varbinary(16)",
+            foreignKey = @ForeignKey(name = "fk_combination_to_health_condition")
+    )
     private HealthCondition healthCondition;
+
+    @Embedded
+    private CombinationLineItems combinationLineItems;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public CombinationTitle getTitle() {
+        return title;
+    }
+
+    public CombinationContent getContent() {
+        return content;
+    }
+
+    public Gender getRecommendedGender() {
+        return recommendedGender;
+    }
+
+    public HealthCondition getHealthCondition() {
+        return healthCondition;
+    }
+
+    public List<CombinationLineItem> getCombinationLineItems() {
+        return combinationLineItems.getCombinationLineItems();
+    }
 }
