aea0f83d02e6f93758c1aebd7fbac9d6db004c82
Kimjunho <widn45@naver.com>
2022-05-09 11:34:35 +0900

    chore(SupplementException): SupplementException 예외 발생(부모로 위임) - Runtime 예외를 실행시켜 errorCode 메시지 전달

--- src/main/java/com/example/nutrient/domain/exception/SupplementException.java
+++ src/main/java/com/example/nutrient/domain/exception/SupplementException.java
@@ -3,13 +3,8 @@ package com.example.nutrient.domain.exception;
 import static com.example.nutrient.domain.exception.SupplementErrorCode.OK;
 
 public class SupplementException extends RuntimeException{
-    private SupplementErrorCode errorCode = OK;
 
     public SupplementException(SupplementErrorCode errorCode) {
-        this.errorCode = errorCode;
-    }
-
-    public String errorMessage(){
-        return errorCode.getMessage();
+        super(errorCode.getMessage());
     }
 }
