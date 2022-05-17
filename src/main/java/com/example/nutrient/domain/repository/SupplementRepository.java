788f01e291355f651d3ed69ed5d54573b4962b97
mandykr <hongseokii92@gmail.com>
2022-05-17 14:23:22 +0900

    refactor(supplement): 파일 중복 해결

--- src/main/java/com/example/nutrient/domain/repository/SupplementRepository.java
+++ src/main/java/com/example/nutrient/domain/repository/SupplementRepository.java
@@ -3,6 +3,7 @@ package com.example.nutrient.domain.repository;
 import com.example.nutrient.domain.Supplement;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface SupplementRepository {
@@ -10,4 +11,6 @@ public interface SupplementRepository {
     Supplement save(Supplement supplement);
 
     List<Supplement> findAllById(Iterable<UUID> ids);
+
+    Optional<Supplement> findById(UUID id);
 }
